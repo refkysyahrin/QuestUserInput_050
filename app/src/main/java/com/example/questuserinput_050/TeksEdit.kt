9af76f5f2412373c2d344fa6803e87698945dc2e
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-31 15:04:26 +0700

    Membuat Row untuk jenis kelamin yang bisa di pilih dan di klik

--- app/src/main/java/com/example/questuserinput_050/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_050/TeksEdit.kt
@@ -2,8 +2,10 @@ package com.example.questuserinput_050
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -43,8 +45,17 @@ fun FormDataDiri(modifier: Modifier
             onValueChange = {
                 textNama = it
             }
-
         )
+        Row {
+            gender.forEach { item ->
+                Row(modifier = Modifier.selectable(
+                    selected = textJK == item,
+                    onClick = {textJK = item }
+                ), verticalAlignment = Alignment.CenterVertically) {
+
+                }
+            }
+        }
 
     }
 
