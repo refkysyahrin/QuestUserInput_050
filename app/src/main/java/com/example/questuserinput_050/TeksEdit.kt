99e8f5258b7c26a045e9bb2b8bfcf731ac9ff34e
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-31 15:18:44 +0700

    Menambahkan garis

--- app/src/main/java/com/example/questuserinput_050/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_050/TeksEdit.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -17,6 +18,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -71,6 +74,12 @@ fun FormDataDiri(modifier: Modifier
             onValueChange = {
                 textAlamat = it
             }
+        )
+
+        Divider(
+            modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.padding_medium), top = dimensionResource(id = R.dimen.padding_medium)),
+            thickness = dimensionResource(id = R.dimen.divider_tipis),
+            color = Color.Red
         )
 
     }
