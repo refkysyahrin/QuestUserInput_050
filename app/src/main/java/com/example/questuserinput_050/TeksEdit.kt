99cbe93ccdb09aeb040e7aeca03bd1cfd3b5eeae
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-31 14:45:55 +0700

    Menambahkan fungsi bernama FormDataDiri

--- app/src/main/java/com/example/questuserinput_050/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_050/TeksEdit.kt
@@ -1,2 +1,10 @@
 package com.example.questuserinput_050
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier
+){
+
+}
