2b2ff7fb184132cdd58b0a6742390f5c3a546b8d
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-01 15:51:43 +0700

    Membuat radio button

--- app/src/main/java/com/example/questuserinput_050/Praktikum5PAM.kt
+++ app/src/main/java/com/example/questuserinput_050/Praktikum5PAM.kt
@@ -1,5 +1,6 @@
 package com.example.questuserinput_050
 
+import androidx.compose.animation.core.withInfiniteAnimationFrameMillis
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -11,21 +12,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -168,8 +173,35 @@ fun Praktikum5(modifier: Modifier){
                 Spacer(modifier = Modifier.height(height = 10.dp))
 
                 Text(
-                    text = stringResource(R.string.)
+                    text = stringResource(id = R.string.jenis_kelamin),
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(top = 10.dp, start = 50.dp),
                 )
+                Row {
+                    gender.forEach { item ->
+                        Row(
+                            modifier = Modifier
+                                .padding(start = 20.dp)
+                                .selectable(
+                                    selected = textJK == item,
+                                    onClick = {textJK = item},
+                                    role = Role.RadioButton
+                                    ),
+
+                            verticalAlignment = Alignment.CenterVertically
+                        ){
+                                RadioButton(
+                                    selected = textJK == item,
+                                    onClick = {
+                                        textJK = item
+                                    })
+                            Text(item)
+                            }
+                    }
+                }
 
 
             }
