a5c3f14498ed9ef27d65875403b2b36d1412735e
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-01 12:46:35 +0700

    Membuat fun bernama FormRegistrasi

--- app/src/main/java/com/example/questuserinput_050/Praktikum5PAM.kt
+++ app/src/main/java/com/example/questuserinput_050/Praktikum5PAM.kt
@@ -1,22 +1,19 @@
 package com.example.questuserinput_050
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import kotlinx.coroutines.sync.Mutex
 import java.util.Date
 
 @Composable
-fun FormRegistrasi() {
-    // Variabel untuk menampung input user dari user
-    var namaLengkap by remember { mutableStateOf("") }
-    var kotaAsal by remember { mutableStateOf("") }
-    var tanggalLahir by remember { mutableStateOf<Date?>(null) }
-    var rt by remember { mutableStateOf("") }
-    var rw by remember { mutableStateOf("") }
-    var jenisKelamin by remember { mutableStateOf("") }
-    var setujuSyarat by remember { mutableStateOf("") }
+fun FormRegistrasi(modifier: Modifier){
 
 }
