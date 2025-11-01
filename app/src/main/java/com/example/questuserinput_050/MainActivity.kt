e525b3a2d96207671a8e74695fd66aaadfbd91c5
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-01 16:42:07 +0700

    menganti fun

--- app/src/main/java/com/example/questuserinput_050/MainActivity.kt
+++ app/src/main/java/com/example/questuserinput_050/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestUserInput_050Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   FormRegistrasi(
+                   Praktikum5(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
